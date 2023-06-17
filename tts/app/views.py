b50d0d1e0b7c2ad069ad4c9f4b2cf3164a2e0d47
Moses Makaka <moseskax@gmail.com>
2023-06-17 10:10:58 +0300

    Update to views.py

--- tts/app/views.py
+++ tts/app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .utils import text_to_speech
 
-# Create your views here.
+def text_to_speech_view(request):
+    # Path to the transcript file
+    transcript_file = 'path/to/transcript.txt'
+
+    # Call the text_to_speech function with the transcript file path
+    output_file = text_to_speech(transcript_file)
+
+    # Return the audio file as the response
+    with open(output_file, 'rb') as file:
+        response = HttpResponse(file.read(), content_type='audio/wav')
+        response['Content-Disposition'] = 'attachment; filename="output.wav"'
+        return response
+
